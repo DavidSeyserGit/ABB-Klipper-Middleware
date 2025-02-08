3bab206a9eb0c3964584baf1c142491c1bf9af76
DavidSeyserGit <96821053+DavidSeyserGit@users.noreply.github.com>
2025-02-08 18:46:23 +0100

    added functionality to send the received data to an endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,56 @@
 use std::net::TcpListener;
-use std::io::Read;
+use std::io::{Read, Write};
+use reqwest;
+use std::error::Error;
+use tokio::runtime::Runtime;
 
-fn main()->Result<(), std::io::Error>{
-    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
-    for stream in listener.incoming(){
-        match stream{
-            Ok(mut stream)=>{
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6969")?;
+    println!("Listening on 127.0.0.1:6969");
+
+    let rt = Runtime::new()?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
                 let mut buffer = [0; 1024];
-                loop{
-                    match stream.read(&mut buffer){
-                        Ok(sz)=>{
-                            let buffer_string = &buffer[..sz-2];
-                            let recieved_data = String::from_utf8_lossy(buffer_string);
-                            
-                            //instead of printing i should handle the data and send it to the
-                            //endpoint
-    
-                            println!("{:?}", recieved_data);
-                        },
-                        Err(_) => todo!()
+                loop {
+                    match stream.read(&mut buffer) {
+                        Ok(sz) => {
+                            if sz == 0 {
+                                // Connection closed
+                                break;
+                            }
+                            let received_data = String::from_utf8_lossy(&buffer[..sz]).to_string();
+                            println!("Received data: {}", received_data);
 
-                    };
+                            let result = rt.block_on(post_to_moonraker(&received_data));
+                            match result {
+                                Ok(response) => println!("Response: {:?}", response),
+                                Err(e) => eprintln!("Error posting to Moonraker: {:?}", e),
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Error reading from stream: {:?}", e);
+                            break;
+                        }
+                    }
                 }
-
             }
-            Err(err)=>{
-                eprintln!("Error: {:?}", err)
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
             }
         }
     }
-    
+
     Ok(())
 }
 
-
-fn handle_client(){
-    todo!()
-//we need to implement the json rpc2 connection 
+async fn post_to_moonraker(data: &str) -> Result<reqwest::Response, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let response = client.post("http://127.0.0.1:7125/printer/gcode/script")
+        .query(&[("script", data)])
+        .send()
+        .await?;
+    Ok(response)
 }
