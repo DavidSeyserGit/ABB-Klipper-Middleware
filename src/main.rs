d4e4d2b1dd82d76406da2816f1d7baaf87f82a9b
DavidSeyserGit <96821053+DavidSeyserGit@users.noreply.github.com>
2025-02-08 17:42:17 +0100

    added tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 use std::net::TcpListener;
-use http_req::request;
-use Vec;
-//creating the listener that listens to the port from the robot
+use std::io::Read;
 
-fn main() {
+fn main()->Result<(), std::io::Error>{
+    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    for stream in listener.incoming(){
+        match stream{
+            Ok(mut stream)=>{
+                let mut buffer = [0; 1024];
+                loop{
+                    match stream.read(&mut buffer){
+                        Ok(sz)=>{
+                            let buffer_string = &buffer[..sz-2];
+                            let recieved_data = String::from_utf8_lossy(buffer_string);
+                            
+                            //instead of printing i should handle the data and send it to the
+                            //endpoint
+    
+                            println!("{:?}", recieved_data);
+                        },
+                        Err(_) => todo!()
 
+                    };
+                }
+
+            }
+            Err(err)=>{
+                eprintln!("Error: {:?}", err)
+            }
+        }
+    }
+    
+    Ok(())
 }
+
 
 fn handle_client(){
     todo!()
