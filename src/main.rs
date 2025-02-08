35d2b1b8cffb13f471b2a92b9247df8532063f27
DavidSeyserGit <96821053+DavidSeyserGit@users.noreply.github.com>
2025-02-08 15:38:38 +0100

    package creation

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::net::TcpListener;
+use http_req::request;
+use Vec;
+//creating the listener that listens to the port from the robot
+
+fn main() {
+
+}
+
+fn handle_client(){
+    todo!()
+//we need to implement the json rpc2 connection 
+}
