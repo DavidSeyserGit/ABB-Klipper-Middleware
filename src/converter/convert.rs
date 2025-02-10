1c1492e68f99cd860dbe79b0cc41cfe08239c385
DavidSeyserGit <96821053+DavidSeyserGit@users.noreply.github.com>
2025-02-10 11:16:17 +0100

    added fuctionality to write the socket commands to a *.mod file.

--- src/converter/convert.rs
+++ src/converter/convert.rs
@@ -2,22 +2,35 @@ use std::env;
 use std::fs;
 use std::error::Error; // Import the Error trait
 use std::process; // For cleaner error handling
+use std::path::Path;
 
-fn main(){
-    let contents = read_file();
-    println!("{:?}", contents);
-}
-
-fn read_file()->Result<String, Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    //getting the parameters from the terminal
     let args: Vec<String> = env::args().collect();
-
     if args.len() < 2 {
         eprintln!("Usage: {} <file_path>", args[0]); // Print to stderr for errors
         process::exit(1); // Exit with a non-zero code to indicate an error
     }
-
     let file_path = &args[1];
+    let path = Path::new(file_path);
 
+    // if let checks if the path has an extension and when it does it holds it
+    if let Some(extension) = path.extension(){
+        if extension == "mod"{
+            //we read a .mod file which is for ABB rapid code
+            let contents = read_file(file_path)?;
+            //add the socket to the string if it doesnt exist already
+            let _ = fs::write(file_path, search_and_create_socket(contents));
+            //we have to change every instance of CallExtruder to a socket  
+        }
+        else{
+            todo!();
+        }
+    }
+    Ok(())
+}
+
+fn read_file(file_path: &String)->Result<String, Box<dyn Error>>{
     match fs::read_to_string(file_path) {
         Ok(contents) => {
             Ok(contents) // Return Ok(()) to indicate success
@@ -27,4 +40,13 @@ fn read_file()->Result<String, Box<dyn Error>>{
             Err(Box::new(e)) // Box the error to satisfy the Result type
         }
     }
+}
+
+fn search_and_create_socket(contents: String) -> String{
+    //we have to check if a socket is already in the file and initialized
+    //if not we have to create a socket on a specifc ip adress and port
+    //manipulate the string so that we add a socket in Rapid-Code Style
+    //return the new string
+    //currently the socket will be created as the first thing
+    format!("VAR socketdev my_socket;\nSocketCreate my_socket;\nSocketConnect my_socket, \"192.168.0.1\", 1025;\n{}", contents)
 }
